0e7892d98cd19390ed1cf4dfc42f4640ffeec0dc
AleksandarKovachev <akovachev@tu-sofia.bg>
2017-04-09 22:22:34 +0300

    Design the scene.

--- app/src/main/java/sofia/tu/panda/ticket/ticketpanda/Activities/ProgramItemActivity.java
+++ app/src/main/java/sofia/tu/panda/ticket/ticketpanda/Activities/ProgramItemActivity.java
@@ -1,9 +1,14 @@
 package sofia.tu.panda.ticket.ticketpanda.Activities;
 
+import android.animation.ObjectAnimator;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,7 +16,7 @@ import sofia.tu.panda.ticket.ticketpanda.Adapters.SwipePageAdapter;
 import sofia.tu.panda.ticket.ticketpanda.Objects.Program;
 import sofia.tu.panda.ticket.ticketpanda.R;
 
-public class ProgramItemActivity extends AppCompatActivity {
+public class ProgramItemActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewPager viewPager;
     private SwipePageAdapter swipePageAdapter;
@@ -52,5 +57,64 @@ public class ProgramItemActivity extends AppCompatActivity {
         buy = (Button) findViewById(R.id.buy_button);
         buy.setText("КУПИ БИЛЕТ ЗА " + program.getPrice() + " лв.");
 
+        buy.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        LayoutInflater li = LayoutInflater.from(this);
+        View dialogView = li.inflate(R.layout.tickets_layout, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setView(dialogView);
+
+        final Button a1, a2, a3, a4, a5, a6;
+        final Button b1, b2, b3, b4, b5, b6, b7;
+        final Button v1, v2, v3, v4, v5, v6, v7, v8;
+        final Button g1, g2, g3, g4, g5, g6, g7, g8, g9;
+        final Button d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
+        final Button e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11;
+
+        final TextView textView = (TextView) dialogView.findViewById(R.id.tickets_price);
+
+        a1 = (Button) dialogView.findViewById(R.id.a1);
+
+        boolean clicked = false;
+
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animate(v);
+                textView.setText(6 + " лв.");
+            }
+        });
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Добави",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                .setNegativeButton("Отмени",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+    }
+
+    private void animate(View v) {
+        ObjectAnimator objectAnimatorButton = ObjectAnimator.ofFloat(v, "rotationY", 0.0f, 360);
+        objectAnimatorButton.setDuration(1000);
+        objectAnimatorButton.start();
     }
 }
